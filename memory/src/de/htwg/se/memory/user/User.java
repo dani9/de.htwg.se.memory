834cd54b1b90ac84163eb2e05842d9c4ce9b89a1
Johannes Waidner <Johannes.Waidner@htwg-konstanz.de>
2014-03-28 13:51:59 +0100

    first draft user

--- memory/src/de/htwg/se/memory/user/User.java
+++ memory/src/de/htwg/se/memory/user/User.java
@@ -1,5 +1,47 @@
 package de.htwg.se.memory.user;
 
 public abstract class User {
+	private String name;
+	private String nickname;
+	private Object type;
+	private int points;
+
+	public User(String name, String nickname, Object type) {
+		this.name = name;
+		this.nickname = nickname;
+		this.type = type;
+	}
+
+	public Object getType() {
+		return type;
+	}
+
+	public void setType(Object type) {
+		this.type = type;
+	}
+
+	public int getPoints() {
+		return points;
+	}
+
+	public void setPoints(int points) {
+		this.points = points;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
 
 }
