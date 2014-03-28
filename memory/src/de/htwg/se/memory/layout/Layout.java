35d9f1af66a229358a4cd7cc0cd5b92f7c6bba01
Daniel <dani92.mue@googlemail.com>
2014-03-28 15:07:30 +0100

    edit Field.java and Layout.java

--- memory/src/de/htwg/se/memory/layout/Layout.java
+++ memory/src/de/htwg/se/memory/layout/Layout.java
@@ -1,5 +1,22 @@
 package de.htwg.se.memory.layout;
 
 public class Layout {
+	public Layout(int columns) {
+	makeLayout(columns);
+	}	
+	private Field matrix[][] = new Field[5][5];
+	
+	public void makeLayout(int columns){
+	matrix[0][0] = new Field("Feld_0_0");
+	matrix[1][0] = new Field("Feld_1_0");	
+	}
+	
+	
+	
+	
+public static void main(String[] args) {
+	Layout test=new Layout(2);
+	System.out.println(test.matrix);
+}
 
 }
