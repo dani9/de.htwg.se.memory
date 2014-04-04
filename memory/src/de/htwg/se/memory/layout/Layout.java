a237e20a0487aa106b93ec66f5e8a68dabd0c5d3
Daniel <dani92.mue@googlemail.com>
2014-04-04 14:33:15 +0200

    auto Fill Layout added mix Method

--- memory/src/de/htwg/se/memory/layout/Layout.java
+++ memory/src/de/htwg/se/memory/layout/Layout.java
@@ -1,24 +1,71 @@
 package de.htwg.se.memory.layout;
 
+import java.util.LinkedList;
+
 public class Layout {
 	public Layout(int column) {
 		creatMatrix(column);
 	}
-	
 	private Field matrix[][];
 	
 	
 	public void creatMatrix(int column){
 		matrix = new Field[column][column];
+		fillMatrix();
+		}
+		
+	private void fillMatrix(){
+		LinkedList<Field> list=new LinkedList<Field>();
+			
+		for (int i = 0; i < ((matrix.length*matrix.length)/2); i++) {
+			Field tmp= new Field(Integer.toString(i));
+			list.add(tmp);
+			list.add(tmp);
+			System.out.println(list.size());
+		}
+			
+			
+		for(int x =0; x < matrix.length; ++x){
+			  for(int y =0; y < matrix.length; ++y){
+					matrix[x][y] =list.getFirst();
+					list.removeFirst();
+			  	}
+		  }
+		System.out.println(matrix.length);
 	}
 	
 	public Field getField(int row,int column) {
 		return matrix[row][column];
 	}
 	
-	public void setField(int row ,int column, Field field) {
-		this.matrix[row][column] = field;
-	}
-		
-
+	public void mix(){
+				  int randX , randY;
+				  Field tmp;
+				  for(int x =0; x < matrix.length; ++x){
+					  for(int y =0; y < matrix.length; ++y){
+				  
+						  randX = (int)(Math.floor(Math.random() * matrix.length));
+						  randY = (int)(Math.floor(Math.random() * matrix.length));
+						  tmp = matrix[x][y]; 
+						  matrix[x][y] = matrix[randX][randY]; 
+						  matrix[randX][randY] =tmp;
+					  	}
+				  }
+		}
+	
+	 @Override
+	public String toString() {
+		StringBuilder matrixString = new StringBuilder();
+		  for(int x =0; x < matrix.length; ++x){
+			  for(int y =0; y < matrix.length; ++y){
+		  
+				  	matrixString.append(matrix[x][y].getFieldId());
+				  	matrixString.append("\t");
+			  	}
+			  	if(x < matrix.length-1){
+			  	matrixString.append("\n");
+			  	}
+		  	}
+		  return matrixString.toString();
+	 	}
 }
--- memory/test/de/htwg/se/memory/layout/LayoutTest.java
+++ memory/test/de/htwg/se/memory/layout/LayoutTest.java
@@ -7,14 +7,20 @@ public class LayoutTest extends TestCase{
 	Field field;
 //	@Before
 	public void setUp() throws Exception{
-		layout = new Layout(5);
+		layout = new Layout(6);
 		field = new Field("FF");
 	}
 	
 //	@Test
 	public void testsetField(){
-		layout.setField(4,4, field);
-		assertEquals(field, layout.getField(4, 4));
+//		layout.setField(4,4, field);
+		assertEquals(layout.getField(4, 4), layout.getField(4, 4));
+		layout.getField(4, 4).setFieldId("Haus");
+		assertEquals("Haus", layout.getField(4, 4).getFieldId());
+		System.out.print(layout + "\n"+ "\n");
+		layout.mix();
+		System.out.print(layout);
 	}
+
 
 }
