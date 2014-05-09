4f9e719b9529b4b23f37cca4df65ed8d6eda10ab
Daniel <dani92.mue@googlemail.com>
2014-05-09 16:22:36 +0200

    fix Layout

--- memory/src/de/htwg/se/memory/layout/Layout.java
+++ memory/src/de/htwg/se/memory/layout/Layout.java
@@ -1,24 +1,79 @@
 package de.htwg.se.memory.layout;
 
+import java.util.LinkedList;
+
 public class Layout {
-	public Layout(int column) {
-		creatMatrix(column);
+	public Layout(int column) throws Exception {
+		if(column % 2 == 0){
+			creatMatrix(column);
+		}
+		else{
+			throw(new IllegalArgumentException("only even numbers"));
+		}
+		
 	}
-	
 	private Field matrix[][];
 	
 	
 	public void creatMatrix(int column){
 		matrix = new Field[column][column];
+		fillMatrix();
+		}
+		
+	private void fillMatrix(){
+		LinkedList<Field> list=new LinkedList<Field>();
+			
+		for (int i = 0; i < ((getColumn()*getColumn())/2); i++) {
+			Field tmp= new Field(Integer.toString(i));
+			list.add(tmp);
+			list.add(tmp);
+		}
+			
+			
+		for(int x =0; x < getColumn(); ++x){
+			  for(int y =0; y < getColumn(); ++y){
+					matrix[x][y] =list.getFirst();
+					list.removeFirst();
+			  	}
+		  }
 	}
 	
 	public Field getField(int row,int column) {
 		return matrix[row][column];
 	}
 	
-	public void setField(int row ,int column, Field field) {
-		this.matrix[row][column] = field;
-	}
-		
+	public void mix(){
+				  int randX , randY;
+				  Field tmp;
+				  for(int x =0; x < getColumn(); ++x){
+					  for(int y =0; y < getColumn(); ++y){
+				  
+						  randX = (int)(Math.floor(Math.random() * getColumn()));
+						  randY = (int)(Math.floor(Math.random() * getColumn()));
+						  tmp = matrix[x][y]; 
+						  matrix[x][y] = matrix[randX][randY]; 
+						  matrix[randX][randY] =tmp;
+					  	}
+				  }
+		}
 
+	public int getColumn(){
+		return matrix.length;
+	}
+	
+	 @Override
+	public String toString() {
+		StringBuilder matrixString = new StringBuilder();
+		  for(int x =0; x < getColumn(); ++x){
+			  for(int y =0; y < getColumn(); ++y){
+		  
+				  	matrixString.append(matrix[x][y].getFieldId());
+				  	matrixString.append("\t");
+			  	}
+			  	if(x < (getColumn()-1)){
+			  	matrixString.append("\n");
+			  	}
+		  	}
+		  return matrixString.toString();
+	 	}
 }
--- memory/test/de/htwg/se/memory/layout/LayoutTest.java
+++ memory/test/de/htwg/se/memory/layout/LayoutTest.java
@@ -1,20 +1,53 @@
 
 package de.htwg.se.memory.layout;
-import junit.framework.TestCase;;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import junit.framework.TestCase;
 public class LayoutTest extends TestCase{
 	
-	Layout layout;
+	Layout layout,layout1;
 	Field field;
-//	@Before
+	@Before
 	public void setUp() throws Exception{
-		layout = new Layout(5);
-		field = new Field("FF");
+		layout = new Layout(8);
+		;
+
 	}
 	
-//	@Test
-	public void testsetField(){
-		layout.setField(4,4, field);
-		assertEquals(field, layout.getField(4, 4));
+	@Test
+	public void testIlligalCollum() throws Exception{
+		try {
+			layout1 = new Layout(7);
+		} catch (IllegalArgumentException e) {
+			// TODO: handle exception
+		}
+	}
+	
+	@Test
+	public void testGetColumn(){
+		
+		assertEquals(8, layout.getColumn());
+	}
+	
+	@Test
+	public void testGetField(){
+;
+		assertEquals(layout.getField(4, 4), layout.getField(4, 4));
+	}
+	
+	@Test
+	public void testMix(){
+		System.out.println(layout);
+		Layout tmp = layout;
+		layout.mix();
+		for(int x =0; x < layout.getColumn(); ++x){
+			  for(int y =0; y < layout.getColumn(); ++y){
+					assertNotSame(layout.toString(), tmp.toString());
+			  	}
+		  }
+		
 	}
 
 }
