2172a8deed440b25ea0e0260568bb505dec9d087
stroh <stroh@pr0.dd-dns.de>
2015-01-10 18:48:23 +0100

    added scoreboard firstlayout; added external jars

---
+++ memory/src/de/htwg/se/memory/gui/ScoreBoard.java
@@ -0,0 +1,20 @@
+package de.htwg.se.memory.gui;
+
+import javax.swing.JPanel;
+
+import de.htwg.se.memory.util.Storage;
+
+public class ScoreBoard extends JPanel{
+
+	private static final long serialVersionUID = -3588532668650810124L;
+	
+	public ScoreBoard() {
+		
+		Storage storage = new Storage();
+		
+		
+	
+	}
+	
+
+}
