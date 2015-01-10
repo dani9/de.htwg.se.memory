20e74e0702baae3e422c4c43d759de2d58c94bd9
stroh <stroh@pr0.dd-dns.de>
2015-01-10 18:07:31 +0100

    added gui menu settings options to set grid

--- memory/src/de/htwg/se/memory/gui/GUI.java
+++ memory/src/de/htwg/se/memory/gui/GUI.java
@@ -1,25 +1,31 @@
 package de.htwg.se.memory.gui;
 
-import javax.swing.JFrame;
+import java.awt.CardLayout;
+import java.awt.Component;
 
-import de.htwg.se.memory.util.IconContainer;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class GUI extends JFrame {
 	
 	private static final long serialVersionUID = -4305625036085082377L;
-
+	public static final int PANEL_START = 0;
+	public static final int PANEL_PLAY = 1;
+	public static final int PANEL_SCORE = 2;
+	private CardLayout mainPanel;
+	private JPanel mainCardPanel;
+	
 	public GUI() {
 		
 		this.setTitle("GUI MEMORY LAYOUT");
 		this.setName("main");
 		
-		Grid grid = new Grid(3, 3,150);
+		mainPanel = new CardLayout();
+		mainCardPanel = new JPanel(mainPanel);
+		
+		this.add(mainCardPanel);
 		
 		this.setJMenuBar(new MenuBar());
-		this.add(grid);
-		
-		grid.setIcon("BTN0", "PIC"+(IconContainer.ACE+IconContainer.SPADE));
-		
 		this.setResizable(false);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.validate();
@@ -31,5 +37,27 @@ public class GUI extends JFrame {
 	public static void main(String[] args) {
 		new GUI();
 	}
+	
+	public void showPanel(int index){
+		mainPanel.show(mainCardPanel,index+"");
+	}
+	
+	public void setPanel(JPanel panel, int index){
+		panel.setName(index+"");
+		mainCardPanel.add(panel, index+"");
+	}
+	
+	public JPanel getPanel(int index){
+		
+		Component[] components = mainCardPanel.getComponents();
+		for (Component component : components) {
+			if((index+"").equals(component.getName())){
+				return (JPanel) component;
+			}
+		}
+		return null;
+	}
+	
+	
 
 }
--- memory/src/de/htwg/se/memory/gui/MenuBar.java
+++ memory/src/de/htwg/se/memory/gui/MenuBar.java
@@ -1,30 +1,53 @@
 package de.htwg.se.memory.gui;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JRadioButtonMenuItem;
 
-public class MenuBar extends JMenuBar{
+import de.htwg.se.memory.util.IconContainer;
+
+public class MenuBar extends JMenuBar {
 
 	private static final long serialVersionUID = 2096553262948206430L;
+	private ButtonGroup radioGroup;
 
 	public MenuBar() {
+
+		IconContainer icons = IconContainer.getInstance();
+
+		JMenu menuGame = new JMenu("Game");
+		JMenuItem itemStart = new JMenuItem("play", icons.getIcon("PIC" + IconContainer.RIGHT));
+		JMenuItem itemExit = new JMenuItem("exit", icons.getIcon("PIC" + IconContainer.EXIT));
+
+		menuGame.add(itemStart);
+		menuGame.addSeparator();
+		menuGame.addSeparator();
+		menuGame.add(itemExit);
+
+		JMenu menuSettings = new JMenu("Settings");
+		radioGroup = new ButtonGroup();
+
+		addRadioOptionToMenu(menuSettings, 8);
+		addRadioOptionToMenu(menuSettings, 16);
+		addRadioOptionToMenu(menuSettings, 32);
 		
-		
-		JMenu menu = new JMenu("File");
-		
-		this.add(menu);
-		
-		
-		
-		
+
+		this.add(menuGame);
+		this.add(menuSettings);
+
 	}
-	
-	
-	
-	
-	
-	
-	
-	
+
+	private void addRadioOptionToMenu(JMenu menuSettings, int option) {
+		JRadioButtonMenuItem radioButton = new JRadioButtonMenuItem(option + "x" + option);
+		radioButton.setName(option + "");
+		radioGroup.add(radioButton);
+		menuSettings.add(radioButton);
+	}
+
+	public int getRadioOption() {
+		return Integer.parseInt(radioGroup.getSelection().getActionCommand());
+	}
 
 }
