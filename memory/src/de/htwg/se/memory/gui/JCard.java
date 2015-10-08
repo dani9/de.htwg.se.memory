4435ba0b93f2068d398fe46bbb5509d3fa970b10
Johannes Waidner <jowaidne@htwg-konstanz.de>
2015-10-08 18:27:31 +0200

    added card and panel info

---
+++ memory/src/de/htwg/se/memory/gui/JCard.java
@@ -0,0 +1,23 @@
+package de.htwg.se.memory.gui;
+
+import javax.swing.JButton;
+
+import de.htwg.se.memory.model.playingField.Field;
+
+public class JCard extends JButton {
+	
+	
+	private static final long serialVersionUID = -585620824813026570L;
+	private Field id;
+	
+	
+	public JCard(Field id) {
+		this.id = id;
+	}
+	
+	public Field getField() {
+		return id;
+	}
+	
+
+}
