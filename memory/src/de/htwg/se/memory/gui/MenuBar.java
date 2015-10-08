37d4091ded1c3aa8012059b8135687545ea09bef
Johannes Waidner <jowaidne@htwg-konstanz.de>
2015-10-08 19:03:41 +0200

    not working radioboxes

--- memory/src/de/htwg/se/memory/gui/MenuBar.java
+++ memory/src/de/htwg/se/memory/gui/MenuBar.java
@@ -1,17 +1,22 @@
 package de.htwg.se.memory.gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.ButtonGroup;
+import javax.swing.JComponent;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JRadioButton;
 import javax.swing.JRadioButtonMenuItem;
 
 import de.htwg.se.memory.util.IconContainer;
 
-public class MenuBar extends JMenuBar {
+public class MenuBar extends JMenuBar implements ActionListener {
 
 	private static final long serialVersionUID = 2096553262948206430L;
-	private ButtonGroup radioGroup;
+	private JRadioButtonMenuItem[] jRadioButtonMenuItems;
 
 	public MenuBar() {
 
@@ -19,35 +24,87 @@ public class MenuBar extends JMenuBar {
 
 		JMenu menuGame = new JMenu("Game");
 		JMenuItem itemStart = new JMenuItem("play", icons.getIcon("PIC" + IconContainer.USER));
+		itemStart.setName("start");
+		itemStart.addActionListener(this);
+
 		JMenuItem itemExit = new JMenuItem("exit", icons.getIcon("PIC" + IconContainer.EXIT));
+		itemExit.setName("exit");
+		itemExit.addActionListener(this);
 
 		menuGame.add(itemStart);
 		menuGame.addSeparator();
 		menuGame.addSeparator();
 		menuGame.add(itemExit);
 
-		JMenu menuSettings = new JMenu("Settings");
-		radioGroup = new ButtonGroup();
-
-		addRadioOptionToMenu(menuSettings, 4);
-		addRadioOptionToMenu(menuSettings, 8);
-		addRadioOptionToMenu(menuSettings, 16);
-		
+		JMenu menuSettings = createSettingsMenu();
+		toggelRadioBoxTO(0);
 
 		this.add(menuGame);
 		this.add(menuSettings);
 
 	}
 
-	private void addRadioOptionToMenu(JMenu menuSettings, int option) {
-		JRadioButtonMenuItem radioButton = new JRadioButtonMenuItem(option + "x" + option);
-		radioButton.setName(option + "");
-		radioGroup.add(radioButton);
-		menuSettings.add(radioButton);
+	private JMenu createSettingsMenu() {
+		JMenu menuSettings = new JMenu("Settings");
+
+		jRadioButtonMenuItems = new JRadioButtonMenuItem[3];
+		for (int i = 0; i < jRadioButtonMenuItems.length; i++) {
+			addRadioOptionToMenu(menuSettings, i, 4);
+		}
+		return menuSettings;
 	}
 
-	public int getRadioOption() {
-		return Integer.parseInt(radioGroup.getSelection().getActionCommand());
+	private void addRadioOptionToMenu(JMenu menuSettings, int radioButtonNr, int option) {
+		jRadioButtonMenuItems[radioButtonNr] = new JRadioButtonMenuItem(option + "x" + option);
+		jRadioButtonMenuItems[radioButtonNr].setName(option + "");
+		jRadioButtonMenuItems[radioButtonNr].addActionListener(this);
+		menuSettings.add(jRadioButtonMenuItems[radioButtonNr]);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		JComponent comp = (JComponent) e.getSource();
+
+		String compName = comp.getName();
+		if ("exit".equals(compName)) {
+			System.exit(0);
+
+		} else if ("start".equals(compName)) {
+
+			// TODO
+			System.out.println("START");
+
+		} else if (comp instanceof JRadioButton) {
+			toggelRadioBoxEvent(comp);
+
+		}
+
+	}
+
+	private void toggelRadioBoxEvent(JComponent component) {
+
+		for (int i = 0; i < jRadioButtonMenuItems.length; i++) {
+			if (component.equals(jRadioButtonMenuItems[i])) {
+				toggelRadioBoxTO(i);
+			}
+		}
+
+	}
+
+	private void toggelRadioBoxTO(int i) {
+		setAllRadioBoxFalse();
+		setRadioBoxTrue(i);
+	}
+
+	private void setRadioBoxTrue(int i) {
+		jRadioButtonMenuItems[i].setSelected(true);
+	}
+
+	private void setAllRadioBoxFalse() {
+		jRadioButtonMenuItems[0].setSelected(false);
+		jRadioButtonMenuItems[1].setSelected(false);
+		jRadioButtonMenuItems[2].setSelected(false);
 	}
 
 }
