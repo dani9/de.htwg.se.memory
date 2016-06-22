524dbf822bf3ebd93c79d4bcc9640459eba19dc1
Daniel Mueller <danmuell@htwg-konstanz.de>
2016-06-22 09:48:01 +0200

    don’t change active player, when a pair is found

--- src/de/htwg/se/memory/controller/Controller.java
+++ src/de/htwg/se/memory/controller/Controller.java
@@ -6,149 +6,149 @@ import de.htwg.se.memory.model.playingField.PlayingField;
 import de.htwg.se.memory.util.observer.IObserver.Topic;
 import de.htwg.se.memory.util.observer.Observable;
 
-
-class Choice{
+class Choice {
 	static int row, column;
 }
 
-public class Controller extends Observable{
+public class Controller extends Observable {
 
 	PlayingField playingField = null;
-	
+
 	Player players[] = new Player[2];
-	
+
 	int activePlayer = 0;
 	int turn = 0;
 
-	
-	public Controller(){
-		
+	public Controller() {
+
 	}
-	
-	public void startGame(int fieldSize, String player1Name, String player2Name){
+
+	public void startGame(int fieldSize, String player1Name, String player2Name) {
 		Player player1 = new User(player1Name, player1Name, this);
 		Player player2 = new User(player2Name, player2Name, this);
 		startGame(fieldSize, player1, player2);
 	}
-	
-	public void startGame(int fieldSize, Player player1, Player player2){
-		
+
+	public void startGame(int fieldSize, Player player1, Player player2) {
+
 		playingField = new PlayingField(fieldSize);
 		playingField.mix();
 		players[0] = player1;
 		players[1] = player2;
 		activePlayer = 0;
 		turn = 0;
-		
+
 		notifyObservers(Topic.NEW_GAME_STARTED);
-		
-//		try {
-//			Thread.sleep(1000);
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+
+		// try {
+		// Thread.sleep(1000);
+		// } catch (InterruptedException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
 		getChoice();
 	}
-	
-	public String getActivePlayerName(){
+
+	public String getActivePlayerName() {
 		return players[activePlayer].getName();
 	}
-	
-	public int getActivePlayerPoints(){		
+
+	public int getActivePlayerPoints() {
 		return players[activePlayer].getPoints();
 	}
-	
-	public String getPlayerName(int playerNumber){
-		return players[playerNumber-1].getName();
+
+	public String getPlayerName(int playerNumber) {
+		return players[playerNumber - 1].getName();
 	}
-	
-	public int getPlayerPoints(int playerNumber){
-		return players[playerNumber-1].getPoints();
+
+	public int getPlayerPoints(int playerNumber) {
+		return players[playerNumber - 1].getPoints();
 	}
-	public Player[] getPlayers(){
-		
+
+	public Player[] getPlayers() {
+
 		return players;
 	}
-	public void nextPlayer(){
+
+	public void nextPlayer() {
 		turn = 0;
 		++activePlayer;
 		activePlayer %= 2;
 		playingField.hideAll();
 		notifyObservers(Topic.NEXT_PLAYER);
-		
-//		try {
-//			Thread.sleep(1000);
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+
+		// try {
+		// Thread.sleep(1000);
+		// } catch (InterruptedException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
 		getChoice();
 	}
-	
-	public int getPlayFieldSize(){
-		
+
+	public int getPlayFieldSize() {
+
 		return playingField.getColumn();
 	}
-	
-	public void getChoice(){
+
+	public void getChoice() {
 		players[activePlayer].getChoice();
 	}
-	
-	public int getTurn(){
+
+	public int getTurn() {
 		return turn;
 	}
-	
-	public Field getField(int row, int column){
-		Field field = playingField.getField(row, column);	
+
+	public Field getField(int row, int column) {
+		Field field = playingField.getField(row, column);
 		return field;
 	}
-	
-	public PlayingField getPlayingField(){
+
+	public PlayingField getPlayingField() {
 		return playingField;
 	}
-	
-	
-	public void waitForChoice(){
+
+	public void waitForChoice() {
 		notifyObservers(Topic.WAIT_FOR_CHOICE);
 	}
-	public void setChoice(int row, int column){
-		
+
+	public void setChoice(int row, int column) {
+
 		Field field = getField(row, column);
-		if(field.isVisible() || field.isGuessed()){
+		if (field.isVisible() || field.isGuessed()) {
 			notifyObservers(Topic.WAIT_FOR_CHOICE);
 			return;
 		}
 		field.setVisble();
-		
-		if(turn == 1){
-			if(field.compareTo(getField(Choice.row, Choice.column)) == 0){
+
+		if (turn == 1) {
+			if (field.compareTo(getField(Choice.row, Choice.column)) == 0) {
 				field.setGuessed(true);
 				getField(Choice.row, Choice.column).setGuessed(true);
 				players[activePlayer].addPoint();
-				
-				if(playingField.isAllGeuessed() == true){
+
+				if (playingField.isAllGeuessed() == true) {
 					notifyObservers(Topic.GAME_FINISHED);
+					return;
+				} else {
+					turn = 0;
+					notifyObservers(Topic.WAIT_FOR_CHOICE);
 					return;
 				}
 			}
 		}
-		
-		
-		
+
 		Choice.row = row;
 		Choice.column = column;
 		notifyObservers(Topic.CHOICE_WAS_MADE);
-		
 
-		
-		if(turn == 0){
+		if (turn == 0) {
 			++turn;
 			getChoice();
-			
-		}else{
+
+		} else {
 			notifyObservers(Topic.WAIT_FOR_NEXT_PLAYER);
-			
+
 		}
 	}
 }
